45b27f8a1d83b0776ff6701ba1feb8f1bceb56dd
Subin <subinbs4a@gmail.com>
2025-08-22 10:25:11 +0300

    Basic starter api

--- WebApp_DeployTest/Program.cs
+++ WebApp_DeployTest/Program.cs
@@ -8,7 +8,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Configure AppSettings
-builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+//builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+builder.Services.AddOptions<AppSettings>()
+    .BindConfiguration(nameof(AppSettings));
 
 var app = builder.Build();
 
@@ -19,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 var summaries = new[]
 {
